60f1585d8e40337d8ec0db45f5079a3afa966688
Anton Khludnev <toni.khludnev@outlook.com>
2023-12-05 21:08:10 +0300

    Добавьте файлы проекта.

---
+++ AuthWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace crmSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtonLoginClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Авторизация в приложении находится в разработке!", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Question);
+        }
+
+        private void HyperlinkLoginGuestClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void HyperlinkRegistrationClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Регистрация в приложении находится в разработке!", "Регистрация", MessageBoxButton.OK, MessageBoxImage.Question);
+        }
+    }
+}
